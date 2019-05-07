e54bc796574ac8d6d2a2f6013b94061cd7d239fc
alejandro <alejandro.tang@utec.edu.pe>
2019-05-07 17:53:22 -0500

    Primer Commit

---
+++ cmake-build-debug/CMakeFiles/solver-Mandarine2312.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver-Mandarine2312.dir/main.cpp.o"
+  "CMakeFiles/solver-Mandarine2312.dir/tester/tester.cpp.o"
+  "CMakeFiles/solver-Mandarine2312.dir/operations/operation.cpp.o"
+  "solver-Mandarine2312.pdb"
+  "solver-Mandarine2312"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver-Mandarine2312.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/solver-Mandarine2312.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for solver-Mandarine2312.
+# This may be replaced when dependencies are built.
